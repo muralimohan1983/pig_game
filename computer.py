ae452c1e8535c86c2a6fadaa0366385f953bf435
muralimohan1983 <murali.mohan.rao@gmail.com>
2024-07-07 16:59:57 -0400

    muralimohan1983
    initial version

---
+++ computer.py
@@ -0,0 +1,71 @@
+import random
+import time
+
+
+def player(uPoints):
+    global rollAgain
+    while True:
+        rollChoice = input("y to roll n to give up chance to computer").lower()
+
+        if rollChoice == "y":
+            progressBar()
+            diceNum = random.randint(1, 6)
+            print(f"\r"  "You have rolled, your dice number is ", diceNum)
+
+            if diceNum == 1:
+                rollAgain = False
+                print("You have rolled a 1 it's now the cpu's turn")
+                break
+            else:
+                uPoints += diceNum
+                print("your total points are", diceNum)
+                continue
+
+        elif rollChoice == "n":
+            print("It's now the computers turn")
+            rollAgain = False
+            break
+
+        return rollAgain, uPoints
+
+
+def cpu(cPoints):
+    global rollAgain
+    print("Now it's my turn I am rolling dice"),
+
+    progressBar()
+
+    diceNum = random.randint(1, 6)
+    print("\r" + "My number is ", diceNum)
+
+    if diceNum == 1:
+        print("oh no!!  I have rolled a one and my points have been reset it is your turn")
+        rollAgain = False
+        player()
+    else:
+        cpuDecission = random.randint(1, 2)
+
+        if cpuDecission == 1:
+            print(" I am rolling again\n\n")
+            cPoints += diceNum
+            rollAgain = True
+        if cpuDecission == 2:
+            print("i don't want to roll again, It's your turn to roll")
+            rollAgain = False
+        return cPoints, rollAgain
+
+
+def progressBar():
+    for i in range(1, 3):
+        print("-", end=" ", flush=True)
+        time.sleep(0.5)
+
+    print("\r", end=" ")
+
+    time.sleep(0.5)
+
+    for i in range(1, 7):
+        print("-", end=" ", flush=True)
+        time.sleep(0.5)
+
+    print("\r", end=" ")
---
+++ main.py
@@ -0,0 +1,27 @@
+import computer
+
+userPoints = 0
+cpuPoints = 0
+mode = input("what mode would you like 1 for computer 2 for multiplayer and q for quit").lower()
+
+if mode == "q":
+    print("you have exited the game")
+    quit()
+elif mode == "1":  # computer
+
+    rollChoice = input("would like to roll first [y/n]").lower()
+
+    if rollChoice == "y":
+        rollAgain, userPoints = computer.player(userPoints)
+
+    elif rollChoice == "n":
+        cpuPoints, rollAgain = computer.cpu(cpuPoints)
+
+    if rollAgain :
+        computer.player(userPoints)
+
+    elif rollAgain:
+        computer.cpu(cpuPoints)
+
+elif mode == "2":  # multiplayer
+    print("multiplayer")
